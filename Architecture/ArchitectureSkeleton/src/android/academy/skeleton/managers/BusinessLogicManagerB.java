57035577cbf959b7759b187cf73f806577e93629
Pedro Vicente <neteinstein@gmail.com>
2014-11-25 11:25:09 +0000

    Update BusinessLogicManagerB.java

--- Architecture/ArchitectureSkeleton/src/android/academy/skeleton/managers/BusinessLogicManagerB.java
+++ Architecture/ArchitectureSkeleton/src/android/academy/skeleton/managers/BusinessLogicManagerB.java
@@ -78,66 +78,36 @@ public class BusinessLogicManagerB extends AbstractManager implements BusinessLo
         context.startService(new Intent(context, GenericService.class));
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see com.vodafone.backupplus.managers.interfaces.SystemEventsInterface#onConnectivityAction(int, int)
-     */
     @Override
     public void onConnectivityAction(int wifiState, int networkState) {
         // TODO Auto-generated method stub
 
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see com.vodafone.backupplus.managers.interfaces.SystemEventsInterface#onSystemEventBatteryChanged(int)
-     */
     @Override
     public void onSystemEventBatteryChanged(int batteryPercentage) {
         // TODO Auto-generated method stub
 
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see com.vodafone.backupplus.managers.interfaces.SystemEventsInterface#onSystemEventPowerConnected()
-     */
     @Override
     public void onSystemEventPowerConnected() {
         // TODO Auto-generated method stub
 
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see com.vodafone.backupplus.managers.interfaces.SystemEventsInterface#onSystemEventPowerDisconnected()
-     */
     @Override
     public void onSystemEventPowerDisconnected() {
         // TODO Auto-generated method stub
 
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see com.vodafone.backupplus.managers.interfaces.SystemEventsInterface#onSystemEventMediaScannerFinished()
-     */
     @Override
     public void onSystemEventMediaScannerFinished() {
         // TODO Auto-generated method stub
 
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see com.vodafone.backupplus.managers.interfaces.SystemEventsInterface#onSystemEventMediaMounted()
-     */
     @Override
     public void onSystemEventMediaMounted() {
         if (mGenericServiceBounded) {
@@ -145,11 +115,6 @@ public class BusinessLogicManagerB extends AbstractManager implements BusinessLo
         }
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see com.vodafone.backupplus.managers.interfaces.SystemEventsInterface#onSystemEventMediaUnmounted()
-     */
     @Override
     public void onSystemEventMediaUnmounted() {
     	if (mGenericServiceBounded) {
@@ -158,33 +123,18 @@ public class BusinessLogicManagerB extends AbstractManager implements BusinessLo
 
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see com.vodafone.backupplus.managers.interfaces.SystemEventsInterface#onSystemEventMediaEject()
-     */
     @Override
     public void onSystemEventMediaEject() {
         // TODO Auto-generated method stub
 
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see com.vodafone.backupplus.managers.interfaces.SystemEventsInterface#onSystemEvenStorageLow()
-     */
     @Override
     public void onSystemEventStorageLow() {
         // TODO Auto-generated method stub
 
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see com.vodafone.backupplus.managers.interfaces.SystemEventsInterface#onSystemEvenStorageOk()
-     */
     @Override
     public void onSystemEventStorageOk() {
         // TODO Auto-generated method stub
